4db18e9d264080380d51a1d0dcce7a4322711f5e
shubhamojha1 <subham.k.ojha@gmail.com>
2026-02-19 17:30:12 +0530

    refactoring for testing

--- server/handlers/auth.py
+++ server/handlers/auth.py
@@ -3,31 +3,33 @@ from server.models.user import User, UserRole
 import logging
 import json
 
+def register_user(users, username, is_admin=False):
+    if any(user.username == username for user in users.values()):
+        return None, "duplicate"
+
+    user = User(username, websocket=None)
+    if is_admin:
+        user.user_role = UserRole.ADMIN
+
+    users[user.user_id] = user
+    return user, None
+
 @route("join")
 async def handle_join(websocket, data, users):
     username = data.get("username")
-    if username in users:
+    is_admin = data.get("admin_flag")
+    user, error = register_user(users, username, is_admin)
+    if error:
+    # if username in users:
         logging.info(f"User {username} already exists!")
         await websocket.send(json.dumps({
             "action": "message",
             "username": username,
             "message_type": "duplicate_user_error"
-        }))
+        })) 
         return
-
-    is_admin = data.get("admin_flag")
-    user = User(username = username, websocket = websocket)
-    if is_admin:
-        user.user_role = UserRole.ADMIN
-    users[user.user_id] = user
-    # username_index[username] = user.user_id
-    # print("----")
-    # for user_id, user in users.items():
-    #     print(user.username)
-    #     print(user.user_id)
-    #     print(user.websocket)
-    #     print(user.user_role.value)
-    # print("----")
+    
+    user.websocket = websocket
     logging.info(f"User {username} joined")
     await websocket.send(json.dumps({
         "action": "join",
