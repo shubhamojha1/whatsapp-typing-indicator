47ae26e9cebf0bf5d034a028d1696185cbf5166f
shubhamojha1 <subham.k.ojha@gmail.com>
2026-02-19 17:29:29 +0530

    changes for better testing

--- server/app.py
+++ server/app.py
@@ -3,20 +3,31 @@ from websockets.asyncio.server import serve
 import logging
 import json
 from server.router import handler
+
+# Registering routes
 import server.handlers.messaging
 import server.handlers.auth
 import server.handlers.typing
 
 logging.basicConfig(level=logging.INFO)
 
-users = {}
+# Better for testing
+# New state for each test, and cleaner
+def create_users_store():
+    return {}
 
-async def websocket_handler(websocket):
+async def websocket_handler(websocket, users):
     await handler(websocket, users)
+
+async def create_server(host="localhost", port=8765):
+    users = create_users_store()
+    return await serve(lambda ws: websocket_handler(ws, users), host, port)
 
 async def main():
     # async with serve(handler, "0.0.0.0", 8765) as server: # listen on all interfaces
-    async with serve(websocket_handler, "localhost", 8765) as server:
+    # async with serve(websocket_handler, "localhost", 8765) as server:
+    #     await server.serve_forever()
+    async with await create_server() as server:
         await server.serve_forever()
 
 
